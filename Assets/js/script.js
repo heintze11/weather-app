8a435097647edd5cf24b589e51fc2b253fb00f52
Levi H <levi.heintzelman@gmail.com>
2022-07-27 20:14:48 -0600

    fix units in api call

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,16 +18,15 @@ function addWeather(event) {
     event.preventDefault()
     var city = document.querySelector("#city-search").value;
     localStorage.setItem("city", event.target.previousElementSibling.value);
-
-    
-    var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1e64d4984f9bd49dd45881e4e3f332ca";
-    // fetch (currentWeather)
-    console.log(apiCurrent);
-
-
+    var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=1e64d4984f9bd49dd45881e4e3f332ca";
     var apiFive = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=1e64d4984f9bd49dd45881e4e3f332ca";
-    // fetch(fiveDay)
-    console.log(apiFive);
+    fetch(apiCurrent)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
+    // fetch(apiFive)
+    // .then((response) => response.json())
+    // .then((data) => console.log(data))
 }
 
 search.addEventListener("click", addWeather);
